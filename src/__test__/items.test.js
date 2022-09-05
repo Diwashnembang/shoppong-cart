4832a813f7f6883dcb4baf36f0e445c2ddac9aca
Diwash Nembang <diwashnembang2@gmail.com>
2022-09-05 22:04:17 +0545

    added add and remove function to update no of items and to update price

--- src/__test__/items.test.js
+++ src/__test__/items.test.js
@@ -1,12 +1,13 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { act } from "react-dom/test-utils";
 import Items from "../components/Items";
 
 const mockItem = {
 
     id: "001",
     src: "src/assets/electronics/geforce-rtx-3080-ti.jpg",
-    price: "Rs 400",
+    price: "400",
     name: "Graphic Card",
 
 
@@ -14,12 +15,15 @@ const mockItem = {
 };
 
 beforeEach(() => {
-    render(<Items item={mockItem} />);
+    act(() => {
+
+        render(<Items item={mockItem} />);
+    })
 });
 
 describe("Items component", () => {
 
-    it("renders properly", () => {
+    it("should renders properly", () => {
         const ImageElement = screen.getByRole("img");
         expect(ImageElement.alt).toEqual("image of Graphic Card");
         const nameElement = screen.getByTestId("selected-item-name");
@@ -29,8 +33,107 @@ describe("Items component", () => {
 
     });
 
-    // it.todo("add no of item", () => { });
+    it("should add no of item by 1", () => {
+        const addElement = screen.getByRole("button", { name: "+" });
+        const inputElement = screen.getByRole("textbox");
+        userEvent.click(addElement);
+        expect(inputElement.value).toBe('2');
 
-    // it.todo("decrese no of item from the cart", () => { });
+    });
 
+    it("should add no of item by 5", () => {
+        const addElement = screen.getByRole("button", { name: "+" });
+        const inputElement = screen.getByRole("textbox");
+        userEvent.click(addElement);
+        userEvent.click(addElement);
+        userEvent.click(addElement);
+        userEvent.click(addElement);
+        userEvent.click(addElement);
+        expect(inputElement.value).toBe('6');
+
+    });
+
+    it("should decrease no of item by 1", () => {
+        const minusElement = screen.getByRole("button", { name: "-" });
+        const inputElement = screen.getByRole("textbox");
+        userEvent.click(minusElement);
+        expect(inputElement.value).toBe('0');
+
+    });
+
+
+    it("should decrease no of item by 5", () => {
+        const minusElement = screen.getByRole("button", { name: "-" });
+        const inputElement = screen.getByRole("textbox");
+        userEvent.click(minusElement);
+        userEvent.click(minusElement);
+        userEvent.click(minusElement);
+        userEvent.click(minusElement);
+        userEvent.click(minusElement);
+        expect(inputElement.value).toBe('-4');
+
+    });
+
+    it("should change the no of items to 5 when 5 is typed", () => {
+        const inputElement = screen.getByRole("textbox");
+        userEvent.clear(inputElement);
+        userEvent.type(inputElement, "5");
+        expect(inputElement.value).toBe("5");
+    })
+    it("should increse the price of the product by 2 when 2 item are present", () => {
+        const inputElement = screen.getByRole("textbox");
+        const priceElement = screen.getByTestId("selected-item-price");
+        userEvent.clear(inputElement);
+        userEvent.type(inputElement, "3");
+        expect(priceElement.textContent).toBe("1200");
+
+    })
+
+    it("should increse the price of the product by 2 when add is clicked one time", () => {
+        const addElement = screen.getByRole("button", { name: "+" });
+        const priceElement = screen.getByTestId("selected-item-price");
+        userEvent.click(addElement);
+        expect(priceElement.textContent).toBe("800");
+
+    })
+
+    it("should change the number of items when the + button is clicked after typing ", () => {
+        const inputElement = screen.getByRole("textbox");
+        const addElement = screen.getByRole("button", { name: "+" });
+        userEvent.clear(inputElement);
+        userEvent.type(inputElement, "2");
+        userEvent.click(addElement);
+        expect(inputElement.value).toBe("3");
+    })
+
+    it(" should change the number of items when the - button is clicked after typing", () => {
+        const inputElement = screen.getByRole("textbox");
+        const minusElement = screen.getByRole("button", { name: "-" });
+        userEvent.clear(inputElement);
+        userEvent.type(inputElement, "3");
+        userEvent.click(minusElement);
+        expect(inputElement.value).toBe("2");
+    })
+
+    it("should increse the price of the product when + is clicked after typing", () => {
+        const addElement = screen.getByRole("button", { name: "+" });
+        const inputElement = screen.getByRole("textbox");
+        const priceElement = screen.getByTestId("selected-item-price");
+        userEvent.clear(inputElement);
+        userEvent.type(inputElement, "2")
+        userEvent.click(addElement);
+        expect(priceElement.textContent).toBe("1200");
+
+    })
+
+    it("should decrease the price of the product when - is clicked after typing", () => {
+        const inputElement = screen.getByRole("textbox");
+        const minusElement = screen.getByRole("button", { name: "-" });
+        const priceElement = screen.getByTestId("selected-item-price");
+        userEvent.clear(inputElement);
+        userEvent.type(inputElement, "4")
+        userEvent.click(minusElement);
+        expect(priceElement.textContent).toBe("1200");
+
+    })
 });
--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,6 +1,27 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import "./items.css"
 const Items = ({ item }) => {
+
+    const [noOfItems, setNoOfItems] = useState(1);
+    const [price, setPrice] = useState(+item.price);
+
+    const add = () => {
+        setNoOfItems(prev => +prev + 1);
+    }
+
+    const remove = () => {
+        setNoOfItems(prev => +prev - 1);
+    }
+
+    const noOfItemChange = (e) => {
+        setNoOfItems(prev => +e.target.value);
+    }
+
+    useEffect(() => {
+        if (noOfItems > 1) {
+            setPrice(prev => +noOfItems * item.price);
+        }
+    }, [noOfItems])
     return (
         <>
             <div id='selected-item'>
@@ -11,13 +32,24 @@ const Items = ({ item }) => {
                     <p>{item.name}</p>
                 </div>
                 <div className="selected-item-price" data-testid="selected-item-price">
-                    {item.price}
+                    {price}
                 </div>
             </div>
             <div id="upadate-no-of-item">
-                <div className="add">+</div>
-                <input type="text" name="no-of-item" id="no-of-item" />
-                <div className="remove">-</div>
+                <div className="add" onClick={add}>
+                    <button type="button">+</button>
+                </div>
+
+                <input type="text"
+                    name="no-of-item"
+                    id="no-of-item"
+                    value={noOfItems}
+                    onChange={noOfItemChange}
+                />
+
+                <div className="remove" onClick={remove} >
+                    <button type="button">-</button>
+                </div>
             </div>
         </>
     )
