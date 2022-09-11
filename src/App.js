86792f2318b68551d2b917499bfd61d20a1ad6fb
Diwash Nembang <diwashnembang2@gmail.com>
2022-09-11 20:54:32 +0545

    added item state and passed it's setter to shop component

--- src/App.js
+++ src/App.js
@@ -6,19 +6,26 @@ import Shop from './components/Shop'
 import Cart from './components/Cart'
 import { useState } from 'react'
 
+const mock={
+
+    id: "001",
+    src: "src/assets/electronics/geforce-rtx-3080-ti.jpg",
+    price: "400",
+    name: "Graphic Card",
+}
 function App() {
-  /* todoNo1 = open cart when clicked */
 
   const [isCartClicked, setIsCartClicked] = useState(false);
+  const [items,setItems] = useState([mock,mock])/* change items throug shop components pass items to cart*/
   return (
     <div className='App'>
       {/* when cart is clicked from nagivation it setIsCartClicked = true */} 
       <Nagivation setIsCartClicked={setIsCartClicked} />  
       {/* carts take items to render item and setIsCartClicked to close cart when closed is clcked */}
-      {isCartClicked ? <Cart items={[]} setIsCartClicked={setIsCartClicked}/> : null}
+      {isCartClicked ? <Cart items={items} setIsCartClicked={setIsCartClicked}/> : null}
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/shop' element={<Shop />} />
+        <Route path='/shop' element={<Shop setItems={setItems}/>} />
       </Routes>
     </div>
   )
