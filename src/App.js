bc17781cf9f35b9849079424274288bd8943590e
Diwash Nembang <diwashnembang2@gmail.com>
2022-09-11 23:46:49 +0545

    added assets to public dit. for image loading

--- src/App.js
+++ src/App.js
@@ -6,26 +6,30 @@ import Shop from './components/Shop'
 import Cart from './components/Cart'
 import { useState } from 'react'
 
-const mock={
 
-    id: "001",
-    src: "src/assets/electronics/geforce-rtx-3080-ti.jpg",
-    price: "400",
-    name: "Graphic Card",
+const mock = {
+  id: '001',
+  src: 'assets/electronics/71Dc5Fh6kFL._AC_UY218_.jpg' ,
+  price: '400',
+  name: 'Graphic Card',
 }
-function App() {
 
-  const [isCartClicked, setIsCartClicked] = useState(false);
-  const [items,setItems] = useState([mock,mock])/* change items throug shop components pass items to cart*/
+function App() {
+  const [isCartClicked, setIsCartClicked] = useState(false)
+  const [items, setItems] = useState([
+    mock,
+  ]) /* change items throug shop components pass items to cart*/
   return (
     <div className='App'>
-      {/* when cart is clicked from nagivation it setIsCartClicked = true */} 
-      <Nagivation setIsCartClicked={setIsCartClicked} />  
+      {/* when cart is clicked from nagivation it setIsCartClicked = true */}
+      <Nagivation setIsCartClicked={setIsCartClicked} />
       {/* carts take items to render item and setIsCartClicked to close cart when closed is clcked */}
-      {isCartClicked ? <Cart items={items} setIsCartClicked={setIsCartClicked}/> : null}
+      {isCartClicked ? (
+        <Cart items={items} setIsCartClicked={setIsCartClicked} />
+      ) : null}
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/shop' element={<Shop setItems={setItems}/>} />
+        <Route path='/shop' element={<Shop setItems={setItems} />} />
       </Routes>
     </div>
   )
--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,58 +1,64 @@
 import React, { useEffect, useState } from 'react'
-import "./items.css"
+import './items.css'
 const Items = ({ item }) => {
+  const [noOfItems, setNoOfItems] = useState(1)
+  const [price, setPrice] = useState(+item.price)
 
-    const [noOfItems, setNoOfItems] = useState(1);
-    const [price, setPrice] = useState(+item.price);
+  const add = () => {
+    setNoOfItems((prev) => +prev + 1)
+  }
 
-    const add = () => {
-        setNoOfItems(prev => +prev + 1);
+  const remove = () => {
+    setNoOfItems((prev) => +prev - 1)
+  }
+
+  const noOfItemChange = (e) => {
+    setNoOfItems((prev) => +e.target.value)
+  }
+
+  useEffect(() => {
+    if (noOfItems > 1) {
+      setPrice((prev) => +noOfItems * item.price)
     }
+  }, [noOfItems])
 
-    const remove = () => {
-        setNoOfItems(prev => +prev - 1);
-    }
 
-    const noOfItemChange = (e) => {
-        setNoOfItems(prev => +e.target.value);
-    }
+  return (
+    <>
+      <div className='selected-item'>
+        <div className='selectd-item-img' data-testid='selectd-item-img'>
+          <img src={`${item.src}`} alt={`image of ${item.name}`} />
+        </div>
+        <div className='selected-item-info' data-testid='selected-item-name'>
+          <p>{item.name}</p>
+        </div>
+        <div className='selected-item-price' data-testid='selected-item-price'>
+          {price}
+        </div>
+      </div>
+      <div className='upadate-no-of-item'>
+        <div className='add'>
+          <button type='button' onClick={add}>
+            +
+          </button>
+        </div>
 
-    useEffect(() => {
-        if (noOfItems > 1) {
-            setPrice(prev => +noOfItems * item.price);
-        }
-    }, [noOfItems])
-    return (
-        <>
-            <div className='selected-item'>
-                <div className="selectd-item-img" data-testid="selectd-item-img">
-                    <img src={`${item.src}`} alt={`image of ${item.name}`} />
-                </div>
-                <div className="selected-item-info" data-testid="selected-item-name">
-                    <p>{item.name}</p>
-                </div>
-                <div className="selected-item-price" data-testid="selected-item-price">
-                    {price}
-                </div>
-            </div>
-            <div className="upadate-no-of-item">
-                <div className="add" >
-                    <button type="button" onClick={add}>+</button>
-                </div>
+        <input
+          type='text'
+          name='no-of-item'
+          className='no-of-item'
+          value={noOfItems}
+          onChange={noOfItemChange}
+        />
 
-                <input type="text"
-                    name="no-of-item"
-                    className="no-of-item"
-                    value={noOfItems}
-                    onChange={noOfItemChange}
-                />
-
-                <div className="remove"  >
-                    <button type="button"onClick={remove}>-</button>
-                </div>
-            </div>
-        </>
-    )
+        <div className='remove'>
+          <button type='button' onClick={remove}>
+            -
+          </button>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Items
