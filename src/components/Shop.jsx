139caeeb7e491a49de1781d16d16d9847c8a2b6d
Diwash Nembang <diwashnembang2@gmail.com>
2022-09-11 20:52:19 +0545

    added set Items props to update items in app component

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
-function Shop() {
+function Shop({setItems}) {
+ /* todo write function to update of App component to update cart  */
     return (
         <div id='shop'>Hello from the shop</div>
     )
