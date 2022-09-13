d7ee79c522fb77d715380f0db9c4a46a2fda4090
Diwash Nembang <diwashnembang2@gmail.com>
2022-09-13 21:24:10 +0545

    added add to cart button to product and styled prodcut added boder , box shadwod

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart'
 
 import './product.css'
 
-const Product = ({ item }) => {
+const Product = ({ item, addToCart }) => {
+  const addToCartClicked = () => {
+    addToCart(item)
+  }
   return (
     <div className='product-card'>
       <div className='product-image'>
         <img src={item.src} alt={item.name} />
       </div>
       <div className='product-name'>{item.name}</div>
-      <div className='product-price'>{item.price}</div>
+      <div className='product-price'>
+        {item.price}
+        <div className='add-product-to-cart' onClick={{ addToCartClicked }}>
+          <AddShoppingCartIcon />
+        </div>
+      </div>
     </div>
   )
 }
