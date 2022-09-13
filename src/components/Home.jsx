ab4b6ac43e4da0445081806ab6a2e288bb4b4924
Diwash Nembang <diwashnembang2@gmail.com>
2022-09-13 15:10:43 +0545

    added home and styled it both desktop and mobile responsive

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+import './home.css'
 
 const Home = () => {
-    return (
-        <div>hello from the Home</div>
-    )
+  return (
+    <>
+      <div id='home-component'>
+        <section className='main-product-info'>
+          <h1>Nvidia GeForce RTX 3080 </h1>
+          <h3>Preorder Now </h3>
+          <Link to={'./Shop'}>
+            <button type='button'>Shop Now</button>
+          </Link>
+        </section>
+        <section className='main-product-image'>
+          <img src='/assets/electronics/geforce-rtx-3080-ti.jpg' />
+        </section>
+      </div>
+    </>
+  )
 }
 
 export default Home
