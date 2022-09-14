61f4d62fbdc58ac518177c1017c150a960135f7f
Diwash Nembang <diwashnembang2@gmail.com>
2022-09-14 19:22:34 +0545

    fixed css changed to grid

--- src/components/SearchProduct.jsx
+++ src/components/SearchProduct.jsx
@@ -1,31 +1,38 @@
-import SearchIcon from '@mui/icons-material/Search';
-import CloseIcon from '@mui/icons-material/Close';
+import SearchIcon from '@mui/icons-material/Search'
+import CloseIcon from '@mui/icons-material/Close'
 
 import React from 'react'
-import "./search.css"
+import './search.css'
 
 const SearchProduct = (props) => {
-    const { showSearch } = props;
-    const handleCloseClick = () => {
-        showSearch(false);
+  const { showSearch } = props
+  const handleCloseClick = () => {
+    showSearch(false)
+  }
+  return (
+    <div
+      id='search'
+      style={{ minHeight: document.documentElement.scrollHeight }}
+    >
+      <section className='close' onClick={handleCloseClick} data-testid='close'>
+        <CloseIcon fontSize='large' />
+      </section>
 
-    }
-    return (
-        <div id='search'>
-            <section className='close' onClick={handleCloseClick} data-testid="close">
-                <CloseIcon fontSize='large' />
-            </section>
-
-            <section className="input">
-                <div className="search-logo">
-                    <SearchIcon fontSize='16' />
-                </div>
-                <input type="text" name="search" id="product-search" placeholder='Search product' />
-
-            </section>
-
+      <section className='input'>
+        <div className='search-logo'>
+          <SearchIcon fontSize='16' />
         </div>
-    )
+        <div className="search-box">
+        <input
+          type='text'
+          name='search'
+          id='product-search'
+          placeholder='Search product'
+        />
+        </div>
+      </section>
+    </div>
+  )
 }
 
 export default SearchProduct
